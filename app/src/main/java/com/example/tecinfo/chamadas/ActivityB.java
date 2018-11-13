7325e3bce8dbca498d0237e5163ff34f2d015f9f
higorbatista <higor.pm13@gmail.com>
2018-11-12 22:38:52 -0200

    fim da aula 12/11/2018

--- app/src/main/java/com/example/tecinfo/chamadas/ActivityB.java
+++ app/src/main/java/com/example/tecinfo/chamadas/ActivityB.java
@@ -1,13 +1,51 @@
 package com.example.tecinfo.chamadas;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityB extends AppCompatActivity {
+    SharedPreferences meusDasdos;
+    public static final String mypreference = "call";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+
+        final EditText emailb = findViewById(R.id.emailb);
+        final EditText senhaB  = findViewById(R.id.senhaB);
+        final EditText csenha = findViewById(R.id.csenha);
+        Button cadastrar = findViewById(R.id.btn_cadastro);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (senhaB.getText().toString().equals(csenha.getText().toString())){
+
+                    SharedPreferences.Editor editor = meusDasdos.edit();
+                    editor.putString("email", emailb.getText().toString());
+                    editor.putString("senha", csenha.getText().toString());
+
+                    editor.commit();
+                    Toast.makeText(getApplicationContext(),
+                            "dados salvos com sucesso!", Toast.LENGTH_LONG).show();
+
+                    Intent intent = new Intent( ActivityB.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "As senhas não conferm", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tecinfo/chamadas/MainActivity.java
+++ app/src/main/java/com/example/tecinfo/chamadas/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.tecinfo.chamadas;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences meusDados;
+    public  static final  String mypreference ="call";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        meusDados = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+
+        Button login = findViewById(R.id.login);
+        TextView registro = findViewById(R.id.registro);
+        final EditText email = findViewById(R.id.emaila);
+        final EditText senha = findViewById(R.id.senhaB);
+
+        login .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String meuEmail = email.getText().toString();
+                String minhaSenha = senha.getText().toString();
+
+                if (meusDados.contains("email") && meusDados.contains("senha")){
+
+                    if (meuEmail.equals(meusDados.getString("email",""))
+                            &&minhaSenha.equals(meusDados.getString("senha", ""))){
+                        Intent intent = new Intent( MainActivity.this,
+                                Activityc.class);
+                        startActivity(intent);
+                        finish();
+                    }else {
+                        Toast.makeText(getApplicationContext(),
+                                "Email e senha invalidos! "Toast.LENGTH_SHORT).show();
+                    }
+
+                }else {
+                    Toast.makeText(getApplicationContext(),
+                            "nao existe cadastro"Toast.LENGTH_SHORT).show();
+                }
+
+                Intent intent =  new Intent(MainActivity.this, Activityc.class);
+                startActivity(intent);
+            }
+        });
     }
 }
